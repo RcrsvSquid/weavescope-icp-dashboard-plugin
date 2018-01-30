b909313bf5a49ae1dd3b18bf7463d1db5913affa
Sidneyw <sidney.wijngaarde1@ibm.com>
2018-01-30 15:29:34 -0500

    Dynamic Host Link Information

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -10,36 +10,16 @@ import (
 	"os/signal"
 	"path/filepath"
 	"syscall"
+
+	weave "github.ibm.com/sidney-wijngaarde1/weave-scope-plugin/weaveplugin"
 )
-
-func setupSocket(socketPath string) (net.Listener, error) {
-	os.RemoveAll(filepath.Dir(socketPath))
-	if err := os.MkdirAll(filepath.Dir(socketPath), 0700); err != nil {
-		return nil, fmt.Errorf("failed to create directory %q: %v", filepath.Dir(socketPath), err)
-	}
-	listener, err := net.Listen("unix", socketPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to listen on %q: %v", socketPath, err)
-	}
-
-	log.Printf("Listening on: unix://%s", socketPath)
-	return listener, nil
-}
-
-func setupSignals(socketPath string) {
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-interrupt
-		os.RemoveAll(filepath.Dir(socketPath))
-		os.Exit(0)
-	}()
-}
 
 func main() {
 	// We put the socket in a sub-directory to have more control on the permissions
-	const socketPath = "/var/run/scope/plugins/scope-test/scope-test.sock"
+	const socketPath = "/var/run/scope/plugins/icp-dashboard/icp-dashboard.sock"
 	hostID, _ := os.Hostname()
+
+	fmt.Printf("Current Host IP %s\n", weave.GetOutboundIP())
 
 	// Handle the exit signal
 	setupSignals(socketPath)
@@ -67,54 +47,63 @@ func main() {
 func Report(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.String())
 
-	raw, err := ioutil.ReadFile("./test.json")
+	var rpt *weave.WeaveReport = &weave.WeaveReport{}
+
+	rpt.Plugins = []weave.PluginSpec{{
+		ID:          "icp-dashboard",
+		Label:       "ICP Dashboard",
+		Description: "Links into the ICP Dashboard",
+		Interfaces:  []string{"reporter"},
+		APIVersion:  1,
+	}}
+
+	var hostNode *weave.Host = &weave.Host{}
+	hostNode.Init()
+
+	rpt.Host = weave.Topology{
+		Nodes: map[string]weave.Node{
+			weave.GetWeaveID(hostNode): weave.Node{
+				Latest: weave.GetLatestURL(hostNode),
+			},
+		},
+		MetadataTemplates: map[string]weave.Metadata{
+			weave.GetMetaDataTableID(hostNode): weave.GetWeaveMetaData(hostNode),
+		},
+		TableTemplates: map[string]weave.Table{
+			hostNode.GetTableID(): weave.GetWeaveTable(hostNode),
+		},
+	}
+
+	raw, err := json.Marshal(rpt)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Fatalf("File read error %v", err)
+		log.Fatalf("JSON Marshall Error %v", err)
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(raw)
 }
 
-// func (p *Plugin) getTopologyHost() string {
-// 	return fmt.Sprintf("%s;<host>", p.HostID)
-// }
+func setupSocket(socketPath string) (net.Listener, error) {
+	os.RemoveAll(filepath.Dir(socketPath))
+	if err := os.MkdirAll(filepath.Dir(socketPath), 0700); err != nil {
+		return nil, fmt.Errorf("failed to create directory %q: %v", filepath.Dir(socketPath), err)
+	}
+	listener, err := net.Listen("unix", socketPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to listen on %q: %v", socketPath, err)
+	}
 
-// type report struct {
-// 	Host struct {
-// 		Nodes struct {
-// 			SidneywWorker2Host struct {
-// 				Latest struct {
-// 					LinkTableMetaData struct {
-// 						Value     string    `json:"value"`
-// 						Timestamp time.Time `json:"timestamp"`
-// 					} `json:"link-table-meta-data"`
-// 				} `json:"latest"`
-// 			} `json:"sidneyw-worker-2;<host>"`
-// 		} `json:"nodes"`
-// 		MetadataTemplates struct {
-// 			LinkMeta struct {
-// 				ID       string  `json:"id"`
-// 				Label    string  `json:"label"`
-// 				DataType string  `json:"dataType"`
-// 				Priority float64 `json:"priority"`
-// 				From     string  `json:"from"`
-// 			} `json:"link-meta"`
-// 		} `json:"metadata_templates"`
-// 		TableTemplates struct {
-// 			LinkTable struct {
-// 				ID     string `json:"id"`
-// 				Label  string `json:"label"`
-// 				Prefix string `json:"prefix"`
-// 			} `json:"link-table"`
-// 		} `json:"table_templates"`
-// 	} `json:"Host"`
-// 	Plugins []struct {
-// 		ID          string   `json:"id"`
-// 		Label       string   `json:"label"`
-// 		Description string   `json:"description"`
-// 		Interfaces  []string `json:"interfaces"`
-// 		APIVersion  string   `json:"api_version"`
-// 	} `json:"Plugins"`
-// }
+	log.Printf("Listening on: unix://%s", socketPath)
+	return listener, nil
+}
+
+func setupSignals(socketPath string) {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-interrupt
+		os.RemoveAll(filepath.Dir(socketPath))
+		os.Exit(0)
+	}()
+}
